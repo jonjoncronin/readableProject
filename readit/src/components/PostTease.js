9d37e0f6515f6d2d994c89c64ccaa97b512f0369
Jon Cronin <jonjoncronin@gmail.com>
2018-04-08 21:28:05 -0700

    Add some input controls for adding, voting, selecting filters

--- readit/src/components/PostTease.js
+++ readit/src/components/PostTease.js
@@ -9,7 +9,11 @@ class PostTease extends Component {
       <div class="w3-card-2 w3-white" style={{ marginBottom: "8px" }}>
         <div class="w3-container w3-cell-row w3-win8-steel">
           <h5 class="w3-cell w3-cell-middle">{post.title}</h5>
-          <div class="w3-cell w3-cell-middle w3-right">u d</div>
+          <div class="w3-cell w3-cell-middle w3-right">
+            <button onClick={() => (console.log("upVote"))}>u</button>
+            {post.voteScore}
+            <button onClick={() => (console.log("downVote"))}>d</button>
+          </div>
         </div>
         <div class="w3-container">{post.body}</div>
       </div>
--- readit/src/components/PostsControl.js
+++ readit/src/components/PostsControl.js
@@ -5,8 +5,22 @@ class PostsControl extends Component {
     console.log(this.props);
 
     return (
-      <div class="w3-card-4 w3-win8-mauve w3-padding w3-margin-bottom">
-        <h3>Posts Header</h3>
+      <div class="w3-card-4 w3-cell-row w3-win8-mauve w3-padding w3-margin-bottom">
+        <h3 class="w3_cell w3-cell-middle">Posts Header</h3>
+        <div class="w3-cell w3-cell-bottom">
+          <select
+            defaultValue="filter"
+            onChange={(event) => (
+              console.log(event.target.value + " selected")
+            )}>
+            <option value="filter" disabled>Filter by...</option>
+            <option value="byDate">date posted</option>
+            <option value="byVote">vote count</option>
+          </select>
+        </div>
+        <div class="w3-cell w3-cell-bottom">
+          <button onClick={() => (console.log("adding post"))}>Add</button>
+        </div>
       </div>
     );
   }
