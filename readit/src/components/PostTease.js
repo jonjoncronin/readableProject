b3275b29ab0ce6f1d294775b0dbdd40b5a7bb378
Jon Cronin <jonjoncronin@gmail.com>
2018-04-15 09:44:49 -0700

    Clean up formatting of the PostTease component

--- readit/src/components/PostTease.js
+++ readit/src/components/PostTease.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class PostTease extends Component {
   render() {
@@ -8,42 +8,51 @@ class PostTease extends Component {
     return (
       <div className="w3-card-2 w3-white" style={{ marginBottom: "8px" }}>
         <div className="w3-container w3-win8-steel">
-          <div className="w3-container w3-padding-small">
-            <h6><Link to={"/" + post.category + "/" + post.id}>{post.title}</Link></h6>
+          <div className="w3-container w3-large">
+            <Link to={"/" + post.category + "/" + post.id}>{post.title}</Link>
           </div>
           <div className="w3-container w3-small">
-            Author: {post.author}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Comments: {post.commentCount}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Votes: {post.voteScore}
+            Date: {new Date(post.timestamp).toDateString()}
+          </div>
+          <div className="w3-container w3-small">
+            Author: {post.author}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Comments: {post.commentCount}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Votes: {post.voteScore}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           </div>
         </div>
         <div className="w3-container w3-win8-olive">
           <button
             id="upVote"
-            onClick={(event) => {
-            handleVoteOnPost(post.id, event.target.id);
+            onClick={event => {
+              handleVoteOnPost(post.id, event.target.id);
             }}
-            className="w3-button">
+            className="w3-button"
+          >
             upVote
           </button>
           <button
             id="downVote"
-            onClick={(event) => {
-            handleVoteOnPost(post.id, event.target.id);
+            onClick={event => {
+              handleVoteOnPost(post.id, event.target.id);
             }}
-            className="w3-button">
+            className="w3-button"
+          >
             downVote
           </button>
           <Link
             to={"/" + post.category + "/" + post.id + "/edit"}
-            className="w3-button">
+            className="w3-button"
+          >
             edit
           </Link>
           <button
             id="deletePost"
-            onClick={(event) => {
+            onClick={event => {
               console.log("handle deleting of post");
               handlePostDelete(post.id);
             }}
-            className="w3-button">
+            className="w3-button"
+          >
             delete
           </button>
         </div>
