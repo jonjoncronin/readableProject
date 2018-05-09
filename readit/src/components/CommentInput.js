2cd4c2b0ab8e96738860455e0c4bce39f326b6a1
Jon Cronin <jonjoncronin@gmail.com>
2018-05-08 23:25:54 -0700

    Create initial CommentInput component

---
+++ readit/src/components/CommentInput.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class CommentInput extends Component {
+  render() {
+    console.log("CommentInput Props", this.props);
+
+    return (
+      <div className="w3-card w3-white w3-padding" style={{marginBottom:'8px'}}>
+        <label>Author</label>
+        <input
+          name="author"
+          className="w3-input w3-light-gray w3-hover-white"
+          type="text"
+          placeholder="Your name..."
+        />
+        <label>Comment</label>
+        <textarea
+          name="comment"
+          className="w3-input w3-light-gray w3-hover-white"
+          placeholder="Your comment..."
+        />
+        <button className="w3-button">Submit</button>
+        <button className="w3-button w3-right">
+          Cancel
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CommentInput;
