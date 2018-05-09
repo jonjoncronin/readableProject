cb53e964ad29a06647992e9cfaed2fdd3758e42e
Jon Cronin <jonjoncronin@gmail.com>
2018-05-08 23:25:17 -0700

    Create generic Accordion component

---
+++ readit/src/components/Accordion.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Accordion extends Component {
+
+  state = { expanded: false };
+
+  handleButtonClick = () => {
+    this.setState({expanded: !this.state.expanded});
+  }
+
+  render() {
+    console.log("Accordion Props", this.props);
+    const { title, content } = this.props;
+
+    return (
+      <div>
+        <div className="w3-button w3-block" onClick={() => this.handleButtonClick()}>
+          {title}
+        </div>
+        <div className={this.state.expanded ? "w3-show": "w3-hide"}>
+          {content}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Accordion;
